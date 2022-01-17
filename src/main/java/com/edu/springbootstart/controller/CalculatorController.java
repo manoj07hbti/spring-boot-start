eb2123c6d64dc24ec3d9455a6ec490918735ee34
60manojkumar@gmail.com <Motivated@2019>
2022-01-17 09:23:05 +0530

    Path Variable ans Request Param

--- src/main/java/com/edu/springbootstart/controller/CalculatorController.java
+++ src/main/java/com/edu/springbootstart/controller/CalculatorController.java
@@ -1,25 +1,25 @@
 package com.edu.springbootstart.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
 
-
-    @RequestMapping("/add")
-    public String  addition(){
-
-        int a=3; int b=13;
+// PATH VARIABLE
+    @RequestMapping("/add/{a}/{b}")
+    public String  addition(@PathVariable int a, @PathVariable int b){
 
         int sum=a+b;
      return  "Addition of a and b is: "+sum;
     }
 
-    @RequestMapping("/sub")
-    public String  subtraction(){
 
-        int a=33; int b=13;
+// REQUEST PARAM
+    @RequestMapping("/sub")
+    public String  subtraction(@RequestParam int a, @RequestParam int b){
 
         int result=a-b;
         return  "Subtraction of a and b is: "+result;
